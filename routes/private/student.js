68682e19e4cfd5fd1f72055d897fe31586d8f56a
Tiffany Love <tnlove_99@yahoo.com>
2017-06-15 16:00:18 -0500

    added student route

---
+++ routes/private/student.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+var path = require('path');
+var bodyParser = require('body-parser');
+var pg = require('pg');
+var port = 3000;
+ //PG
+ var config = {
+  database: '',
+  host: 'localhost',
+  port: port,
+  max: 20
+};// end config
+var pool = new pg.Pool ( config );
+
+
+router.get('/',function(req,res){
+  console.log('in student slips');
+  var studentSlips = [];
+
+  pool.connect(function(err, connection, done){
+    if(err){
+      console.log(err);
+      res.send(400);
+    }
+    else{
+      var resultSet = connection.query('SELECT * FROM users JOIN slips ON user.id=slips.student_id');
+
+      resultSet.on('row', function(row){
+        studentSlips.push(row);
+      });
+      resultSet.on('end', function(){
+        done();
+        console.log(studentSlips);
+        res.send(studentSlips);
+      });
+    }
+  });
+}); // end router.get
+
+
+router.post('/', function(req,res){
+console.log('in router post');
+var data = req.body;
+pool.connect(function(err, connection, done){
+  if(err){
+    console.log(err);
+    res.send(400);
+  }
+  else{
+    connection.query( 'INSERT INTO slips (slip_number, s, o, a, r, date_entered, student_id) VALUES ($1, $2, $3, $4, $5, $6, $7) ');
+    done();
+    res.send(200);
+  }
+});
+});// end router.post
+
+module.exports = router;
