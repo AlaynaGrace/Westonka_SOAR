84433f62e058a9e1f4dd01bf864eb703cd816700
Andrew Hastings <droo.hastings@gmail.com>
2017-06-21 10:24:42 -0500

    teacher route updates

--- public/scripts/controllers/teacher.controller.js
+++ public/scripts/controllers/teacher.controller.js
@@ -1,14 +1,36 @@
-googleAuthApp.controller('teacherController', function(){
+googleAuthApp.controller('teacherController', ['$http','$scope','$timeout', 'AuthFactory',function($http, $scope, $timeout, AuthFactory){
 console.log('teacher');
 var vm = this;
+var authFactory = AuthFactory;
 //start getStudentList
 vm.getStudentList = function() {
-  $http({
-    method: 'GET',
-    url:'/teacher'
-  }).then(function(response){
-    vm.studentArray = response.data;
+  // $http({
+  //   method: 'GET',
+  //   url:'/teacher'
+  // }).then(function(response){
+  //   vm.studentArray = response.data;
+  // });
+  authFactory.isLoggedIn()
+  .then(function (response) {
+    if (response.data.status) {
+      _this.displayLogout = true;
+      authFactory.setLoggedIn(true);
+      console.log('response from auth factory', response.data);
+      _this.username = response.data.name;
+    } else { // is not logged in on server
+      _this.displayLogout = false;
+      authFactory.setLoggedIn(false);
+    }
   });
 };//end of getStudentList
 
-});//end myApp
+
+
+
+}]);//end myApp
+// $http({
+//   method: 'GET',
+//   url:'/teacher'
+// }).then(function(response){
+//   vm.studentArray = response.data;
+// });
--- routes/private/teacher.js
+++ routes/private/teacher.js
@@ -4,35 +4,29 @@
  */
 var express = require('express');
 var router = express.Router();
-const pg = require('pg');
-let port = 3000;
+var pool = require('../../modules/pool.js');
 
-var config = {
- database: '',
- host: 'localhost',
- port: port,
- max: 20
-};// end config
+
 
 router.get('/', function (req, res) {
   console.log('hit teacher route on server');
-  // pool.connect(function ( err, connection, done){
-  //   if (err) {
-  //     res.send( 400 );
-  //   } else {
-  //     //replace this with actualy query
-  //     let resultSet = connection.query("SELECT * FROM users WHERE homeroom_id =$1, [req.user.homeroom_id]");
-  //        var userArray = [];
-  //       resultSet.on('row', function(row){
-  //       userArray.push(row);
-  //     }); //end on row
-  //       resultSet.on('end', function(){
-  //       done();
-  //       console.log(userArray);
-  //       res.send(userArray);
-  //     });
-  //   }//end else
-  // });// end pool connect
+  pool.connect(function ( err, connection, done){
+    if (err) {
+      res.send( 400 );
+    } else {
+      //replace this with actualy query
+      let resultSet = connection.query("SELECT * FROM users homerooms");
+         var userArray = [];
+        resultSet.on('row', function(row){
+        userArray.push(row);
+      }); //end on row
+        resultSet.on('end', function(){
+        done();
+        console.log(userArray);
+        res.send(userArray);
+      });
+    }//end else
+  });// end pool connect
   res.sendStatus(200);
 
 });
