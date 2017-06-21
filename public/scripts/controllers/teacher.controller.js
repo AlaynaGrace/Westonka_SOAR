cd1019473fd6c438624c6d8a6c3cea0ad1ad946a
Andrew Hastings <droo.hastings@gmail.com>
2017-06-21 16:58:25 -0500

    teacher route complete

--- public/scripts/controllers/teacher.controller.js
+++ public/scripts/controllers/teacher.controller.js
@@ -2,35 +2,37 @@ googleAuthApp.controller('teacherController', ['$http','$scope','$timeout', 'Aut
 console.log('teacher');
 var vm = this;
 var authFactory = AuthFactory;
+
+
 //start getStudentList
+authFactory.isLoggedIn()
+.then(function (response) {
+  if (response.data) {
+    vm.displayLogout = true;
+    authFactory.setLoggedIn(true);
+    console.log('this is response.data',response.data.email);
+    vm.email = response.data.email;
+  } else { // is not logged in on server
+    vm.displayLogout = false;
+    authFactory.setLoggedIn(false);
+  }
+});
+
+
+//hook this up to button on student page to get class list
 vm.getStudentList = function() {
-  // $http({
-  //   method: 'GET',
-  //   url:'/teacher'
-  // }).then(function(response){
-  //   vm.studentArray = response.data;
-  // });
-  authFactory.isLoggedIn()
-  .then(function (response) {
-    if (response.data.status) {
-      _this.displayLogout = true;
-      authFactory.setLoggedIn(true);
-      console.log('response from auth factory', response.data);
-      _this.username = response.data.name;
-    } else { // is not logged in on server
-      _this.displayLogout = false;
-      authFactory.setLoggedIn(false);
-    }
-  });
-};//end of getStudentList
-
-
-
-
-}]);//end myApp
-// $http({
-//   method: 'GET',
-//   url:'/teacher'
-// }).then(function(response){
-//   vm.studentArray = response.data;
-// });
+  console.log('hit getStudentList');
+  console.log('email in getStudentList', vm.email);
+  var objectToSend = {
+    email: vm.email
+  };
+$http({
+      method: 'POST',
+      url:'/teacher',
+      data: objectToSend
+    }).then(function(response){
+      console.log('response.data', response.data);
+      vm.studentArray = response.data;
+    });
+  };//end of getStudentList
+}]);
--- routes/private/teacher.js
+++ routes/private/teacher.js
@@ -2,32 +2,38 @@
  * Handles requests for teacher view data.
  * @module private/teacher
  */
-var express = require('express');
-var router = express.Router();
-var pool = require('../../modules/pool.js');
+ var express = require('express');
+ var app = express();
+ var router = express.Router();
+ var path = require('path');
+ var bodyParser = require('body-parser');
+ var pool = require('../../modules/pool.js');
 
 
 
-router.get('/', function (req, res) {
-  console.log('hit teacher route on server');
+router.post('/', function (req, res) {
+  console.log('req.body on server /teacher', req.body.email);
+  var userEmail = req.body.email;
   pool.connect(function ( err, connection, done){
     if (err) {
+      console.log(err);
       res.send( 400 );
     } else {
       //replace this with actualy query
-      let resultSet = connection.query("SELECT * FROM users homerooms");
+      // var resultSet = connection.query("SELECT * FROM users JOIN homerooms ON users.homeroom_id=homerooms.id WHERE users.email=$1 AND teacher=NULL", [userEmail]);
+        var resultSet = connection.query("SELECT * from users WHERE homeroom_id=(SELECT homeroom_id from users WHERE email =$1)", [req.body.email]);
          var userArray = [];
         resultSet.on('row', function(row){
         userArray.push(row);
       }); //end on row
         resultSet.on('end', function(){
         done();
-        console.log(userArray);
+        console.log('userArray', userArray);
         res.send(userArray);
       });
     }//end else
   });// end pool connect
-  res.sendStatus(200);
+
 
 });
 
